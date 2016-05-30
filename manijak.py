017024b472265bb05118a743324b755bd040badc
Ahmed <ahmed.popovic@gmail.com>
2016-05-30 02:48:59 +0200

    Dodao linewraping na displeju i novu komandu

--- manijak.py
+++ manijak.py
@@ -21,6 +21,8 @@ import signal
 import sys
 import os
 
+import textwrap
+
 # Konfiguracija za displej
 DC = 24
 RST = 25
@@ -31,6 +33,11 @@ SPI_DEVICE = 0
 def draw_ellipse(disp, r, g, b, x, y, precnik):
     draw = disp.draw()
     draw.ellipse((x, y, precnik, precnik), outline=(r,g,b), fill=(r,g,b))
+    disp.display()
+
+def draw_rect(disp, r, g, b, x1, y1, x2, y2):
+    draw = disp.draw()
+    draw.rectangle((x1, y1, x2, y2), outline=(r, g, b), fill=(r, g, b))
     disp.display()
 
 def draw_rotated_text(image, text, position, angle, font, fill=(255,255,255)):
@@ -89,11 +96,25 @@ class StdOutListener(StreamListener):
         tvit = json.loads(data)
         
         if tracking == True:
-            print("%s kaze \"%s\"" % (tvit["user"]["name"], tvit["text"]))
-            staPrikazati = "%s kaze \"%s\"" % (tvit["user"]["name"], tvit["text"])
-            disp.clear()
+            #print("%s kaze \"%s\"" % (tvit["user"]["name"], tvit["text"]))
+            #staPrikazati = "%s kaze \"%s\"" % (tvit["user"]["name"], tvit["text"])
+            #disp.clear()
+            #draw_rotated_text(disp.buffer, staPrikazati, (125, 50), 90, font, fill=(255,255,255))
+            #disp.display()
+
+            # Prvo prikazi ime
+            staPrikazati = "%s kaze: " % (tvit["user"]["name"])
             draw_rotated_text(disp.buffer, staPrikazati, (125, 50), 90, font, fill=(255,255,255))
+
+            # Otprilike moze stati 60 slova?
+            staSvePrikazati = textwrap.wrap(tvit["text"], 60)
+            plusY = 20
+            for prikaziMe in staSvePrikazati:
+                draw_rotated_text(disp.buffer, prikaziMe, (125, 50+plusY), 90, font, fill=(255,255,255))
+                plusY = plusY + 20
+                
             disp.display()
+            
             komanda = tvit["text"]
             if tvit["text"].find("::") != -1:
                 parsiraj_komandu(tvit["text"].replace("#etf2016us", "").strip(), tvit["user"]["screen_name"])
@@ -169,6 +190,24 @@ def krug(parametri, komeOdgovoriti):
     precnik = int(parametri[5].strip())
 
     draw_ellipse(disp, r, g, b, x, y, precnik)
+
+def rect(parametri, komeOdgovoriti):
+    global disp
+    
+    if len(parametri) != 8:
+        print("@%s Neispravan broj parametara!" % komeOdgovoriti)
+        api.update_status(status=("@%s Neispravan broj parametara!" % komeOdgovoriti))
+        return
+
+    r = int(parametri[0].strip())
+    g = int(parametri[1].strip())
+    b = int(parametri[2].strip())
+    x1 = int(parametri[3].strip())
+    y1 = int(parametri[4].strip())
+    x2 = int(parametri[5].strip())
+    y2 = int(parametri[5].strip())
+
+    draw_rect(disp, r, g, b, x1, y1, x2, y2)
     
 def rgb_ledica(parametri, komeOdgovoriti):    
     if len(parametri) != 1:
@@ -207,7 +246,8 @@ def rgb_ledica(parametri, komeOdgovoriti):
 
 funkcije = {"ledica": ledica,
             "rgb_ledica": rgb_ledica,
-            "krug": krug    
+            "krug": krug,
+            "rect": rect
 }
         
 def menu():
@@ -219,7 +259,7 @@ def menu():
     print("1 - Tvitajte nesto")
     print("2 - Pratite tvitove")
     print("3 - Izlaz")
-    print("4 - Nacrtaj elipsu")
+    print("4 - Nacrtaj elipsu (debug)")
     mod = input("Unesite izbor: ") # Treba validirati ovaj ulaz
 
     if str(mod) == "1":
